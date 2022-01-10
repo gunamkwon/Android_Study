685a2d61ba96e3ffc4fc92d43014ec25afc666a1
Gunam Kwon <rnska96@mju.ac.kr>
2022-01-11 01:33:50 +0900

    타이머 구현

--- Timer/app/src/main/java/com/example/timer/MainActivity.kt
+++ Timer/app/src/main/java/com/example/timer/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.timer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.SeekBar
 import com.example.timer.databinding.ActivityMainBinding
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    private var currentCountDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,39 +20,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.seekbarTimer.apply {
-            max = 3599
+            max = 60
             setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(
                     seekBar: SeekBar?,
                     progress: Int,
                     fromUser: Boolean
                 ) {
-                    var second = progress % 60
-                    var minute = progress / 60
-                    binding.apply {
-                        binding.textSecond.text = if(second < 10) "0${second}" else "${second}"
-                        binding.textMinute.text = if(minute < 10 )"0${progress / 60}" else "${progress / 60}"
-                    }
+                    if(fromUser)
+                        updateRemainingTime(progress * 60 * 1000L)
                 }
 
                 override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                    var second = progress % 60
-                    var minute = progress / 60
-                    binding.apply {
-                        binding.textSecond.text = if(second < 10) "0${second}" else "${second}"
-                        binding.textMinute.text = if(minute < 10 )"0${progress / 60}" else "${progress / 60}"
-                    }
+                    currentCountDownTimer?.cancel()
+                    currentCountDownTimer = null
                 }
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                    var second = progress % 60
-                    var minute = progress / 60
-                    binding.apply {
-                        binding.textSecond.text = if(second < 10) "0${second}" else "${second}"
-                        binding.textMinute.text = if(minute < 10 )"0${progress / 60}" else "${progress / 60}"
-                    }
+                    seekBar ?: return
+                    currentCountDownTimer = createCountDownTimer(seekBar.progress * 60 * 1000L)
+                    currentCountDownTimer?.start()
                 }
             })
         }
+    }
+
+    private fun createCountDownTimer(initialMillis: Long): CountDownTimer =
+        object: CountDownTimer(initialMillis, 1000L){
+            override fun onTick(millisUntilFinished: Long) {
+                updateRemainingTime(millisUntilFinished)
+                updateSeekBar(millisUntilFinished)
+            }
+            override fun onFinish() {
+                updateRemainingTime(0)
+                updateSeekBar(0)
+            }
+        }
+
+    private fun updateRemainingTime(remainMillis: Long) {
+        val remainSeconds = remainMillis / 1000
+        binding.textMinute.text = "%02d".format(remainSeconds / 60)
+        binding.textSecond.text = "%02d".format(remainSeconds % 60)
+    }
+
+    private fun updateSeekBar(remainMillis: Long) {
+        binding.seekbarTimer.progress = (remainMillis / 1000 / 60).toInt()
     }
 }
