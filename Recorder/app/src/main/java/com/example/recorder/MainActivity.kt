e2412230f26daf5d98157fe75d96c96d99333bd9
Gunam Kwon <rnska96@mju.ac.kr>
2022-01-19 01:31:16 +0900

    오디오 권한 진행중

--- Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
+++ Recorder/app/src/main/java/com/example/recorder/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.recorder
 
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.security.Permissions
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.btn_record)
     }
 
+    private val PERMISSION_REQUEST = 100
     private var state = State.BEFORE_RECORDING
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +27,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initViews()
+
+
+        onCheckPermission()
+    }
+
+
+    fun onCheckPermission() {
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) == PERMISSION_GRANTED) {
+            // 권한이 있으니 실행
+        } else {
+            requestRecordingPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+
+    fun requestRecordingPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.RECORD_AUDIO)) {
+            Toast.makeText(this, "마이크 접근 권한이 요구됩니다",Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), PERMISSION_REQUEST)
+        } else {
+            Toast.makeText(this, "카메라 허가를 받을 수 없습니다", Toast.LENGTH_SHORT).show()
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.RECORD_AUDIO), PERMISSION_REQUEST)
+        }
     }
 
     fun initViews() {
