769a7c1f8c97ecf8fb5ae2760fe949d3a1273128
Gunam Kwon <rnska96@mju.ac.kr>
2022-01-23 23:20:43 +0900

    웹브라우저 기본 기능 완료

--- SimpleWebApp/app/src/main/java/com/example/simplewebapp/MainActivity.kt
+++ SimpleWebApp/app/src/main/java/com/example/simplewebapp/MainActivity.kt
@@ -1,11 +1,122 @@
 package com.example.simplewebapp
 
+import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private val webView: WebView by lazy {
+        findViewById(R.id.webView)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy {
+        findViewById(R.id.progressBar)
+    }
+    private val refreshLayout: SwipeRefreshLayout by lazy {
+        findViewById(R.id.refreshLayout)
+    }
+
+    private val addressBar: EditText by lazy {
+        findViewById(R.id.addressBar)
+    }
+
+    private val goHomeButton: ImageButton by lazy {
+        findViewById(R.id.btn_home)
+    }
+
+    private val goBackButton: ImageButton by lazy {
+        findViewById(R.id.btn_back)
+    }
+
+    private val goForwardButton: ImageButton by lazy {
+        findViewById(R.id.btn_front)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+        initViews()
+    }
+
+    override fun onBackPressed() {
+        if(webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
+        webView.apply {
+            webViewClient = WebViewClient() // 내 앱에 WebView Loading
+            webChromeClient = WebChromeClient()
+            settings.javaScriptEnabled = true // JavaScript 코드 허용 ( 웹뷰의 이벤트 가능해짐)
+            loadUrl(DEFAULT_URL)
+        }
+    }
+
+    private fun bindViews() {
+        addressBar.setOnEditorActionListener { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE) {
+                webView.loadUrl(v.text.toString())
+            }
+            return@setOnEditorActionListener false
+        }
+
+        goBackButton.setOnClickListener {
+            webView.goBack()
+        }
+
+        goForwardButton.setOnClickListener {
+            webView.goForward()
+        }
+
+        goHomeButton.setOnClickListener {
+            webView.clearHistory()
+            webView.loadUrl(DEFAULT_URL)
+        }
+
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+    }
+
+    inner class WebViewClient: android.webkit.WebViewClient() {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient() {
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_URL = "https://www.google.com"
     }
 }
